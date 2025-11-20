7d0221ce1c1ae8f6727341bc348d625eea86c866
Primitiveman495 <158346475+Primitiveman495@users.noreply.github.com>
2025-11-20 19:23:00 +0000

    Renderer + Engine Setup

---
+++ script.js
@@ -0,0 +1,30 @@
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Composite = Matter.Composite,
+    Composites = Matter.Composites,
+    Common = Matter.Common,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    Bodies = Matter.Bodies;
+
+// Creating Engine
+var engine = Engine.create(),
+    world = engine.world;
+
+// Creating Gravity
+engine.gravity.y = 1
+
+// Creating Renderer Box
+var render = Render.create({
+        element: document.body,
+        engine: engine,
+        options: {
+            width: 800,
+            height: 600,
+            showAngleIndicator: true
+        }
+    });
+
+// Run the Renderer
+Render.run(render);
